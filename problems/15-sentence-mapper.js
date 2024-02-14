1bd26815ce3f17e18ae710b024695d9ee4b41de7
sybil <jordan.sybil@hotmail.com>
2024-02-13 20:21:15 -0800

    15 complete

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -26,7 +26,13 @@ console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
 let sentenceMapper = function (sentence, cb) {
-  // Your code here 
+   let words = sentence.split(' ');
+      let newWords = [];
+        for(let i =0; i < words.length; i++){
+           let word = words[i];
+            newWords.push(cb(word));
+        }
+                return newWords.join(' ');
 };
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
